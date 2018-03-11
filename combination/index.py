2056075d376c1c247b7088f8ba30c93c77e261c7
duzitong <duzitong811@126.com>
2018-03-12 01:18:03 +0800

    fix stupid code

--- combination/index.py
+++ combination/index.py
@@ -1,77 +1,78 @@
 
 # coding: utf-8
 
-# In[51]:
+# In[4]:
 
-import tushare
 import pandas as pd
 import numpy as np
 
 
-# In[52]:
+# In[5]:
 
 hs300 = pd.read_csv('399300.csv', encoding="gb2312")
 cy = pd.read_csv('399006.csv', encoding="gb2312")
 
 
-# In[53]:
+# In[65]:
 
 def get_avg_and_std(series):
-    return series.sum()/len(series), series.std()
+    return np.average(series), series.std()
 
 
-# In[137]:
+# In[7]:
 
 hc, cc = hs300['收盘价'][:1889], cy['收盘价']
 
 
-# In[124]:
+# In[8]:
 
 ha, hs = get_avg_and_std(hc)
 
 
-# In[125]:
+# In[9]:
 
 ca, cs = get_avg_and_std(cc)
 
 
-# In[126]:
+# In[10]:
 
 h, c = (hs/ha), (cs/ca)
 h, c
 
 
-# In[148]:
+# In[66]:
 
-def search(sa, sb, begin, end):
-    if end <= begin or end < 0.00001:
-        return begin
+def search(begin, end, sli):
+    if abs(end-begin)<=0.01:
+        a,s = get_avg_and_std(hc[:sli] * begin + cc[:sli] * (1-begin))
+        return a, s, begin, end
     mid = (begin + end) / 2
-    a1, s1 = get_avg_and_std(sa)
-    a2, s2 = get_avg_and_std(sb)
+    a1, s1, b1, e1 = search(begin, mid, sli)
+    a2, s2, b2, e2 = search(mid, end, sli)
     if s1/a1 < s2/a2:
-        return search(sa, sa * (1-mid) + sb * mid, begin, mid)
+        return a1, s1, b1, e1
     else:
-        return search(sa * (1-mid) + sb * mid, sb, mid, end)
+        return a2, s2, b2, e2
     
 
 
-# In[155]:
+# In[86]:
 
-for i in range(50, 1850, 50):
-    print(i, search(hc[:i], cc[:i], 0, 1))
-
-
-# In[156]:
-
-best, sli = 0.5, 50
-a, b = get_avg_and_std(hc[:sli] * (1-best) + cc[:sli] * best)
-b/a
+bests = []
+for i in range(50, 801, 50):
+    _,_,_, end = search(0, 1, i)
+    bests.append(end)
 
 
-# In[157]:
+# In[87]:
 
-13300*best / (1-best)
+hac = [b*2 for b in bests]
+cac = [1-b for b in bests]
+
+
+# In[88]:
+
+print(np.average(hac) / (np.average(hac)+np.average(cac)))
 
 
 # In[ ]:
